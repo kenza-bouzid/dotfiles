322bbfb8cf1b9856492671ecbed432e7a45e52c1
Fernando <fepegar@gmail.com>
2017-08-28 18:26:00 +0100

    Improve style

--- batcharge.py
+++ batcharge.py
@@ -13,22 +13,22 @@ p = subprocess.Popen(["ioreg", "-rc", "AppleSmartBattery"], stdout=subprocess.PI
 output = p.communicate()[0]
 # print(type(output))
 
-o_max = [l for l in output.splitlines() if 'MaxCapacity' in l][0]
-o_cur = [l for l in output.splitlines() if 'CurrentCapacity' in l][0]
-connected = [l for l in output.splitlines() if 'ExternalConnected' in l][0].split()[-1] == 'Yes'
-
-b_max = float(o_max.rpartition('=')[-1].strip())
-b_cur = float(o_cur.rpartition('=')[-1].strip())
+for line in output.splitlines():
+    if 'MaxCapacity' in line:
+        b_max = float(line.rpartition('=')[-1].strip())
+    elif 'CurrentCapacity' in line:
+        b_cur = float(line.rpartition('=')[-1].strip())
+    elif 'ExternalConnected' in line:
+        connected = line.split()[-1] == 'Yes'
 
 charge = b_cur / b_max
 charge_threshold = int(math.ceil(10 * charge))
 
 # Output
 filled_connected = u'▸'
-filled_disconnected = u'◂'
 empty_connected = u'▹'
+filled_disconnected = u'◂'
 empty_disconnected = u'◃'
-
 
 total_slots, slots = 10, []
 if connected:

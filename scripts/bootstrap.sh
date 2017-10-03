cbfeb10078943d017cf55b01b80e2928ec5ca4ff
Fernando <fepegar@gmail.com>
2017-10-03 11:19:23 +0100

    Add comment to bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -140,6 +140,7 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 # Enable remote login
 sudo systemsetup -setremotelogin on
 
+# Add contact info to lock screen
 telephone='+00 0000 000000'
 email='fernando.garcia.17@ucl.ac.uk'
 sudo -S defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Email: ${email}\nTel: ${telephone}"

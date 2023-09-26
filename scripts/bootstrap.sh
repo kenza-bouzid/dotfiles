b92eac88355ad513c678f1f52e0ddff86c69e1aa
Fernando Pérez-García <fepegar@gmail.com>
2023-09-26 23:31:12 +0100

    Add Zoom and fix URL

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -34,6 +34,7 @@ brew install --cask skype
 brew install --cask slack
 brew install --cask vlc
 brew install --cask visual-studio-code
+brew install --cask zoom
 brew install --cask zotero
 
 # poppler includes pdf2images
@@ -51,7 +52,7 @@ brew install \
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/MichaelAquilina/zsh-you-should -use.git $ZSH_CUSTOM/plugins/you-should-use
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 telephone='+00 0000 000000'

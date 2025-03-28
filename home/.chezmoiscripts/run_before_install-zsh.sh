173182e8ccf2966742b1b4125cec287459915d60
Fernando Pérez-García <fepegar@gmail.com>
2025-03-28 09:39:54 +0000

    Add exec zsh to .bash_profile if chsh does not work

--- home/.chezmoiscripts/run_before_install-zsh.sh
+++ home/.chezmoiscripts/run_before_install-zsh.sh
@@ -6,4 +6,10 @@ then
   sudo apt-get install -y zsh
 fi
 
-sudo chsh -s /bin/zsh $USER
+# Changing the shell might not work on the sandbox
+if sudo chsh -s /bin/zsh $USER; then
+  echo "Default shell changed to zsh successfully."
+else
+  echo "Failed to change default shell. Adding fallback to ~/.bash_profile."
+  echo 'exec zsh' >> ~/.bash_profile
+fi
